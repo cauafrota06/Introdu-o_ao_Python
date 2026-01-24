05407261f1395a495c7fa06a4d720a4378f8e0dc
unknown <cauafrotagomes@gmail.com>
2026-01-24 01:05:51 -0400

    Gerenciador de tarefas POO

---
+++ Gerenciador de tarefas POO.py
@@ -0,0 +1,74 @@
+class Tarefa:
+    def __init__(self, id, categoria, descricao, urgencia, diasRestantes):
+        self.id = id
+        self.categoria = categoria
+        self.descricao = descricao
+        self.urgencia = urgencia
+        self.diasRestantes = diasRestantes
+        self.status = "Pendente"
+
+class GerenciadorTarefas:
+    def __init__(self):
+
+        self.lista_tarefas = []
+
+    def adicionar_tarefa(self):
+        tarefa_id = len(self.lista_tarefas) + 1
+        categoria = input('Categoria: ')
+        descricao = input('Descrição: ')
+        urgencia = input('Urgência (Moderado, Importante, Crítico): ')
+        dias_restantes = input('Dias restantes: ')
+
+        nova_tarefa = Tarefa(tarefa_id, categoria, descricao, urgencia, dias_restantes)
+        self.lista_tarefas.append(nova_tarefa)
+
+        print(f"Tarefa '{descricao}' criada com sucesso!")
+
+    def alterar_status(self):
+        if not self.lista_tarefas:
+            print("Nenhuma tarefa cadastrada.")
+            return
+
+        id_busca = int(input("Digite o ID da tarefa: "))
+
+        for tarefa in self.lista_tarefas:
+            if tarefa.id == id_busca:
+                tarefa.status = input("Novo status: ")
+                print(f"Tarefa {id_busca} atualizada!")
+                return
+
+        print("Tarefa não encontrada.")
+
+    def listar_tarefas(self):
+        if not self.lista_tarefas:
+            print("A lista está vazia.")
+        else:
+            print("\n--- LISTA DE TAREFAS ---")
+            for t in self.lista_tarefas:
+                print(f"ID: {t.id} | [{t.status}] {t.descricao} (Urgência: {t.urgencia})")
+
+    def menu(self):
+
+        while True:
+            print('\n1 - Criar Tarefa')
+            print('2 - Alterar Status')
+            print('3 - Listar Tarefas')
+            print('0 - Sair')
+
+            opcao = int(input('Opção: '))
+
+            if opcao == 1:
+                self.adicionar_tarefa()
+
+            elif opcao == 2:
+                self.alterar_status()
+
+            elif opcao == 3:
+                self.listar_tarefas()
+
+            elif opcao == 0:
+                print("Saindo...........")
+                break
+
+gerenciador = GerenciadorTarefas()
+gerenciador.menu()
